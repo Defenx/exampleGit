d1fc1459fbdf9aa092fc6a9b4ac77c9b63a9dd79
PiatraAlatyr <maxisakov96@gmail.com>
2024-12-13 20:25:09 +0200

    Создал функцию для вычисления площади прямоугольника.

--- src/Rectangle.java
+++ src/Rectangle.java
@@ -1,4 +1,20 @@
 public class Rectangle {
     private int a;
     private int b;
+
+    public void setB(int b) {
+        this.b = b;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+
+    public int getB() {
+        return b;
+    }
+
+    public int getA() {
+        return a;
+    }
 }
